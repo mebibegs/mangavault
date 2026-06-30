ebd10633056a864ab5bed243157f36ef20150d49
mebibegs <bibegdemon12@gmail.com>
2026-06-30 18:25:48 +0530

    rm urls

--- fix-urls.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/lib/scrapers/registry.ts', 'utf8');
-
-// FlameComics root browse page is actually /series according to standard, 
-// let's try the generic homepage if the directory doesn't work, 
-// or /browse as we saw in the links list.
-code = code.replace(/https:\/\/flamecomics\.xyz\/manga\/\?page=\${page}/g, 'https://flamecomics.xyz/browse');
-
-fs.writeFileSync('src/lib/scrapers/registry.ts', code);
